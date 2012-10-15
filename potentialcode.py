e2e9f2d10af2d3f81c6fe87ee43a858558dbc686
Jack Pope <Jack@jack-popes-macbook-4.local>
2012-10-15 15:52:17 +0100

    Merge conflict resolved

--- potentialcode.py
+++ potentialcode.py
@@ -2,22 +2,11 @@ from numpy import *
 from pylab import * 
 from scipy import special
 
-N = 100
-r_min = 0.001
-r_max = 25.0
-r_0 = 1.0
-
-r = zeros((N))  
-
-ivals = array(range(0, N))
-spaceri = math.log(r_max/r_min) * (1.0/(N - 1.0))
-r = r_min * exp(spaceri * ivals)
-
 
 def coulombkernel(r): 
-	N = len(r)
-	M = zeros((N,N))
-	for i in range (0,N):
+    N = len(r)
+    M = zeros((N,N))
+    for i in range (0,N):
 		r_i = r[i]
 		print "Coulomb kernel:", i, "/", N
 		for j in range (0,N):
@@ -27,7 +16,8 @@ def coulombkernel(r):
   				    Dr = 0.5 * (r[j + 1] - r_j)
 			        else:
 				    Dr = 0.5 * (r_j - r[j - 1])
-				#Not sure if i understand what simplifications you mean. I have done what i assume you mean below but am unsure of the benefit.
+				#Not sure if i understand what simplifications you mean. 
+                                #I have done what i assume you mean below but am unsure of the benefit.
 				#Ldr    = 2.0 * Dr * math.log(1.0 / Dr**2)
 				#Lplus  = (2.0 * r_j + Dr) * math.log(2.0 * r_j + Dr)
 				#Lminus = (2.0 * r_j - Dr) * math.log(2.0 * r_j - Dr)
@@ -60,14 +50,26 @@ def coulombkernel(r):
 	return M
 
 
+N = 100
+r_min = 0.001
+r_max = 25.0
+r_0 = 1.0
 
-M0 = array(M)			      
-import numpy
-numpy.save("Mx.dat", M0)
+r = zeros((N))  
+
+ivals = array(range(0, N))
+spaceri = math.log(r_max/r_min) * (1.0/(N - 1.0))
+r = r_min * exp(spaceri * ivals)
+
+M = coulombkernel(r)
 rho_j = 1.0 / (1.0 + r**2)**1.5				
 phi_i = dot(M, rho_j) 
+U = 1.0 / ( 1.0 + r**2)**0.5 
 
 
+plot(r, phi_i)
+plot(r, U)
+savefig("image1.pdf")
 
 U = 1.0 / ( 1.0 + r**2)**0.5 
 
