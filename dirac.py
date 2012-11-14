528e96c82ae08eec0d80755097b83b091fe8f90d
Jack Pope <Jack@jack-popes-macbook-4.local>
2012-11-14 16:35:00 +0000

    Matrix has been changed, evaluating the off diagonal elements of the sublattices individually (+/- in the sigma matrix) using the H_0 term derived previously.

--- dirac.py
+++ dirac.py
@@ -4,25 +4,35 @@ from scipy import linalg
 import cmath
 import math
 
-N = 1
-i = cmath.sqrt(-1.0)
+N = 3
 H = np.zeros((2*N,2*N), dtype=complex)
 rho = 3.0
 m = 1
+h = 1
+k = 1
+j = 1j
 
-for y in range (0,2*N):
-    for x in range (0,y):
-        if (x+y)%2 != 0:
-            H[x,y] = -i*((m + 0.5)/rho)
-            H[y,x] = scipy.conj(H[x,y])
+a = math.sin(k*h) / h
+b =((math.sin(((h * k)/2))))
+print a
+print b
+
+for x in range (0,N):
+    for y in range (0,3):
+        H[2*x,2*y]=0
+        H[(2*x)+1,(2*y)]= a - j*b
+        H[(2*x),(2*y)+1]= a + j*b
+  
+  
+  
 print H
-print scipy.linalg.eigh(H)
-
-#test = np.array([(0, i),(-i, 0)])
+#print scipy.linalg.eigh(H)
 
 
-#print test
-#print scipy.linalg.eigh(test)
+
+
+
+
 
 
 
