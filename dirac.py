ecc49b5ae038578b9287522b5b31c61970acc274
Jack Pope <Jack@jack-popes-macbook-4.local>
2012-11-15 11:45:01 +0000

    me and scott have worked on dirac.py it now does... a thing.
    We have added the grid spacing you used in iteration.py
    We then used the formula for staggered fermions and stuck
    that in for the differential part of the dirac eq.
    
    The first d/dx isn't right as we did r[i +1]  r[i] but this
    may not matter too much.
    
    Later we will look into selecting the middle range of the
    eigenvalues.

--- dirac.py
+++ dirac.py
@@ -4,29 +4,36 @@ from scipy import linalg
 import cmath
 import math
 
-N = 3
+N = 10
 H = np.zeros((2*N,2*N), dtype=complex)
 rho = 3.0
 m = 1
-h = 1
+r = np.zeros((N))
 k = 1
 j = 1j
+rmin = 1
+rmax = 10
 
-a = math.sin(k*h) / h
-b =((math.sin(((h * k)/2))))
-print a
-print b
+
+for i in range (0,N):
+    r[i] = rmin * math.exp(math.log(rmax/rmin)/(N - 1.0) * i) 
 
 for x in range (0,N):
-    for y in range (0,3):
+    for y in range (0,N):
+        if y == 0:
+            h = r[y+1] - r[y]
+        else:
+            h = r[y] - r[y-1]
+
+        a = math.sin(k*h) / h
+        b = 2.0 / h * (math.sin(k * h / 2.0)**2)
+
         H[2*x,2*y]=0
         H[(2*x)+1,(2*y)]= a - j*b
         H[(2*x),(2*y)+1]= a + j*b
-  
-  
-  
-print H
-#print scipy.linalg.eigh(H)
+          
+#print H
+print scipy.linalg.eigh(H)
 
 
 
