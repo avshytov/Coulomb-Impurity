9c5d5480f12974ae7ee9267b97be1b461509e81f
scott-taylor-condmat <srt207@exeter.ac.uk>
2013-02-20 09:20:54 +0000

    Theoretical LDOS generated and plotted.

--- ldos.py
+++ ldos.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import scipy
+from scipy import special
 import matplotlib
 from matplotlib import pylab
 from pylab import *
@@ -22,16 +24,21 @@ def ldoscalc():
         dosmat = dostens[m,:,:]
         ldosmat += np.dot(cdmat, dosmat)
     ldosmat = np.transpose(ldosmat)
-    ldosmat = ldosmat[:,:] # / (2 * np.pi * r) ### INTRODUCE h
+    ldosmat = ldosmat[:,:] 
     np.save("ldosmat",ldosmat)
     return ldosmat
 
 def ldosplot(ldosmat):
+    mlist = np.load("mlist.npy")
+    cdtens = np.load("cdtens.npy")
     r = np.load("rvec.npy")
     dr = np.load("drvec.npy")
+    nm = len(mlist)
     nldosmat = 2 * np.pi * dr * r * ldosmat
     E = np.load("Evec.npy")
-    ldostot = np.zeros(len(E))
+    ldostot = np.zeros((len(E)))
+    nu = np.zeros((len(E)))
+    nu_m = np.zeros((len(E)))
     print "Producing colour plot..."
     if False:
         pcolor(r,E,ldosmat, vmin=0.0, vmax=0.15)
@@ -46,8 +53,8 @@ def ldosplot(ldosmat):
     figure()
     plot(E, ldostot, label='Global Density of States')
     legend()
-    rq = 0.05
-    rp = 0.25
+    rq = 0.1
+    rp = 0.5
     ivals = [t[0] for t in enumerate(E) if t[1] > rq and t[1] < rp]
     xvals = [E[t] for t in ivals]
     yvals = [ldostot[t] for t in ivals]
@@ -55,26 +62,46 @@ def ldosplot(ldosmat):
     print "gradient of ldost", grad1[0]
     show()
 
-
-
     for rs in [1.0, 2.0, 3.0, 4.0]:
-        si = int (rs / 25.0 * 500)
+        si = int (rs / 25.0 * 300)
         ri = r[si]
-        print rs, si, ri
-        
         plot (E, ldosmat[:, si], label='r = %g' % ri) 
         ra = 0.5
         rb = 1.5
         ivals = [t[0] for t in enumerate(E) if t[1] < rb and t[1] > ra]
         xvals = [E[t] for t in ivals]
         yvals = [ldosmat[t, si] for t in ivals]
-        plot (xvals, yvals, label='slice %f' %si)
+#        plot (xvals, yvals, label='slice %f' %si)
         grad = np.polyfit(xvals, yvals, 1)
         print grad[0], "for r =", ri 
     xlim(-2.5, 2.5)
     legend()
-    show()
+    #show()
 
+    if True:
+#        figure()
+        print "Calculating analytical LDOS"
+        for rs in [1.0, 2.0, 3.0, 4.0]:
+            si = int (rs / 25.0 * 300)
+            ri = r[si]
+            kr = abs(E) * ri
+            nu = zeros ((shape(E)))
+            for m in range (0,nm):
+                jm = special.jn(abs(mlist[m]),kr)
+                jm1 = special.jn(abs(mlist[m]+1),kr)
+                nu_m = abs(E) / 2.0 / np.pi * (jm**2 + jm1**2)
+                nu += nu_m
+            plot(E, nu, label='Theory Slice r= %g' %ri)
+            ra = 0.1
+            rb = 0.5
+            ivals = [t[0] for t in enumerate(E) if t[1] < rb and t[1] > ra]
+            xvals = [E[t] for t in ivals]
+            yvals = [nu[t] for t in ivals]
+            grad = np.polyfit(xvals, yvals, 1)
+            print grad[0], "for theory r =", ri
+    xlim(-2.5, 2.5)
+    legend()
+    show()
 
     return 0
 
