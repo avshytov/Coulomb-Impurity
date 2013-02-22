46b82e3ca4c3157da4a9842dfeeb752599078e68
scott-taylor-condmat <srt207@exeter.ac.uk>
2013-02-22 14:58:07 +0000

    Comparisons now made between LDOS's.
    Compare theoretical LDOS with simulated LDOS. Compare LDOS with a
    potential to a free particle LDOS and one shifted by the local
    potential value.

--- ldos.py
+++ ldos.py
@@ -25,29 +25,26 @@ def ldoscalc():
         ldosmat += np.dot(cdmat, dosmat)
     ldosmat = np.transpose(ldosmat)
     ldosmat = ldosmat[:,:] 
-    np.save("ldosmat",ldosmat)
+    np.save("ldosmat-U=4r-N=300-rmax=25",ldosmat)
     return ldosmat
 
 def ldosplot(ldosmat):
+    ldos0 = np.load("ldosmat-U=0-N=300.npy")
     mlist = np.load("mlist.npy")
     cdtens = np.load("cdtens.npy")
     r = np.load("rvec.npy")
     dr = np.load("drvec.npy")
+    pot = np.load("potvec.npy")
     nm = len(mlist)
     nldosmat = 2 * np.pi * dr * r * ldosmat
     E = np.load("Evec.npy")
     ldostot = np.zeros((len(E)))
     nu = np.zeros((len(E)))
     nu_m = np.zeros((len(E)))
-    print "Producing colour plot..."
-    if False:
-        pcolor(r,E,ldosmat, vmin=0.0, vmax=0.15)
-        colorbar()
-        ylim(-10.0, 10.0)
-        xlim(0.0, 12.5)
-        show()
-        figure()
-
+    rmax = r[len(r)-1]
+    samps = np.arange(rmax -1) + 1
+    print samps
+    
     for r1 in range (0, len(r)):
         ldostot[:] += nldosmat[:, r1]
     figure()
@@ -60,29 +57,38 @@ def ldosplot(ldosmat):
     yvals = [ldostot[t] for t in ivals]
     grad1 = np.polyfit(xvals, yvals, 1)
     print "gradient of ldost", grad1[0]
-    show()
-
-    for rs in [1.0, 2.0, 3.0, 4.0]:
-        si = int (rs / 25.0 * 300)
-        ri = r[si]
-        plot (E, ldosmat[:, si], label='r = %g' % ri) 
-        ra = 0.5
-        rb = 1.5
-        ivals = [t[0] for t in enumerate(E) if t[1] < rb and t[1] > ra]
-        xvals = [E[t] for t in ivals]
-        yvals = [ldosmat[t, si] for t in ivals]
-#        plot (xvals, yvals, label='slice %f' %si)
-        grad = np.polyfit(xvals, yvals, 1)
-        print grad[0], "for r =", ri 
-    xlim(-2.5, 2.5)
-    legend()
-    #show()
+#    show()
 
     if True:
-#        figure()
+        print "Plotting slices..."
+        for rs in samps:
+            si = int (rs / r[(len(r)-1)] * len(r))
+            ri = r[si]
+            shift = pot[si]
+            figure()
+            plot (E, ldosmat[:, si], label='LDOS r = %g' % ri)
+            plot (E, ldos0[:, si], label='Free LDOS')
+            plot ((E + shift), ldos0[:,si], label='High E shift')
+            # ra = 0.5
+            # rb = 1.5
+            # ivals = [t[0] for t in enumerate(E) if t[1] < rb and t[1] > ra]
+            # xvals = [E[t] for t in ivals]
+            # yvals = [ldosmat[t, si] for t in ivals]
+            # plot (xvals, yvals, label='slice %f' %si)
+            # grad = np.polyfit(xvals, yvals, 1)
+            # print grad[0], "for r =", ri 
+            xlim(-2.5,2.5)
+            legend()
+        show()
+
+    if False:
         print "Calculating analytical LDOS"
-        for rs in [1.0, 2.0, 3.0, 4.0]:
-            si = int (rs / 25.0 * 300)
+        for rs in samps:
+            if True: #(int(rs)%4) == 0:
+                xlim(-1.5, 1.5)
+                legend()
+                figure()
+            si = int (rs / r[(len(r)-1)] * len(r))
             ri = r[si]
             kr = abs(E) * ri
             nu = zeros ((shape(E)))
@@ -91,7 +97,8 @@ def ldosplot(ldosmat):
                 jm1 = special.jn(abs(mlist[m]+1),kr)
                 nu_m = abs(E) / 2.0 / np.pi * (jm**2 + jm1**2)
                 nu += nu_m
-            plot(E, nu, label='Theory Slice r= %g' %ri)
+            plot (E, ldosmat[:, si], label='Simulated r = %g' % ri)
+            plot(E, nu)
             ra = 0.1
             rb = 0.5
             ivals = [t[0] for t in enumerate(E) if t[1] < rb and t[1] > ra]
@@ -102,6 +109,15 @@ def ldosplot(ldosmat):
     xlim(-2.5, 2.5)
     legend()
     show()
+
+    if False:
+        print "Producing colour plot..."
+        pcolor(r,E,ldosmat, vmin=0.0, vmax=0.2)
+        colorbar()
+        ylim(-10.0, 10.0)
+        xlim(0.0, 12.5)
+        show()
+        figure()
 
     return 0
 
