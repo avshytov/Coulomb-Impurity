002b438d4dd167c9c170b6f89597bd1f6b408b77
scott-taylor-condmat <srt207@exeter.ac.uk>
2013-06-14 19:19:03 +0100

    Charge density saved and r and E grids loaded specifically for N.

--- magcheck.py
+++ magcheck.py
@@ -11,23 +11,25 @@ import time
 
 rmin = 0.01
 rmax = 25.0
-r = np.load("rvec.npy")
+r = np.load("rvec-N=200.npy")
+E = np.load("Evec-N=200.npy")
 N = len(r)
 pot = zeros((N))
 info = np.zeros((2))
 a = 1
-Ustr = 1.3
+Ustr = 1.5
 info[0] = Ustr
 #mlist = zeros((2*a + 1))
 mlist = np.array(range(0,a))
-#mlist[0] = 0
+mlist[0] = 0
 for i in range (0,N):
    pot[i] = -Ustr / r[i]
 np.save("mlist",mlist)
 np.save("potvec",pot)    
 Bs = [] 
 for Q in range (0,106):
-   Bs.extend([0.03**2 * Q**2])
+   Bs.extend([(0.03**2 * Q**2)])
+np.save('Bs-%d' %len(Bs), Bs)
 samps = [1.0]# [0.25, 0.5, 0.75, 1.0]
 lines = 21
 allvals = np.zeros((lines,len(Bs), len(samps)))
@@ -41,7 +43,8 @@ for b in range (0, len(Bs)):
     info[1] = B
     np.save("EMinfo", info)
     Ematp, Ematn, cdtens = diracham(r,pot,mlist,B)
-    E, dostens = DOS(Ematp, Ematn, mlist ,r)
+    np.save('chargedenstens-N=%d-ms=%d-U=%g-B=%g' %(N,a,Ustr,B) ,cdtens)
+    Es, dostens = DOS(Ematp, Ematn, mlist ,r)
     x = E
     ldosmat = ldoscalc()
 #    ldosplot(ldosmat)
