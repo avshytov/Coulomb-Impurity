03dbf2ff5466cac54f3c9449c9b8c8c21118e661
Andrey V. Shytov <avshytov@gmail.com>
2013-11-21 15:28:49 +0000

    Added mkrpa2.py : RPA kernel
    This module calculates the kernel that described RPA response
    for a given grid. The function RPA_inter(r) gives the kernel
    for E_F = 0, and the function RPA_intra(r, kF) calculates
    the _extra_ contribution due to finite Fermi energy.
    The kernels are automagically saved in data files to avoid recalculations.
    
    The module also provides testing routines which give some insight about
    the response to "topped off" Coulomb potential.

---
+++ mkrpa2.py
@@ -0,0 +1,388 @@
+import numpy as np
+import math
+from scipy import integrate, interpolate, special, linalg
+
+
+def Pi_intra (q):
+    """
+       Regular part of the polarization operator. The full 
+       formula for polarization operator is
+           Pi = q/16 + Pi_intra * kF / (2.0 * math.pi)
+       At large values of q, this quantity decays which results
+       in regular behaviour at r = r'
+    """
+    if (q <= 2): return (1.0 - math.pi/8.0 * q) 
+    sq = math.sqrt(1.0 - 4.0/q/q)
+    asin = math.asin(2.0/q)
+    return (1.0 - 0.5 * sq - q/4.0*asin) 
+
+def Pi_full(q, kF):
+    return Pi_intra(q/kF) * kF / 2.0 / math.pi + q/16.0
+
+def Pi_intra2 (q):
+    """
+       Pi_intra with asymptotic behaviour, 2/3q^2 subtracted. 
+       I have changed 1/q^2 to 1/(q^2 + 1), because (a) it is finite 
+       at q=0, and (b)the integra for the latter can be easily computed
+    """
+    return Pi_intra(q) - 2.0 / 3.0 / (q**2 + 1.0)
+
+
+def show_pi():
+   qvals = np.arange(0.001, 5.0, 0.01)
+   qvals1 = np.arange(1.00, 5.0, 0.01)
+   
+   Pi_intra_vals = np.vectorize(Pi_intra)(qvals)
+   Pi_full_vals  = np.vectorize(lambda q: Pi_full(q, 1.0))(qvals) 
+   q2inv = np.vectorize(lambda q: 2.0/3.0/(q**2 + 1.0))(qvals)
+   import pylab
+   pylab.plot (qvals, Pi_intra_vals, label='Pi_intra')
+   pylab.plot (qvals, Pi_full_vals, label='Pi_full')
+   pylab.plot (qvals, q2inv, label='2/3q**2')
+   pylab.plot(qvals, Pi_intra_vals - q2inv, label='diff')
+   pylab.legend()
+   pylab.show()
+
+
+def F_intra2(r1, r2, kF):
+    #print r1, r2, kF
+    """
+        Integration routine --- for internal use in Q_intra
+    """
+    s = np.max([r1 * kF, r2 * kF, 1.0])
+    #
+    # First, handle the difference between Pi_intra 
+    # and 2/3 1/(q^2 + 1)
+    #
+    def f(x):
+        J1 = special.jn(0, x * kF * r1 / s)
+        J2 = special.jn(0, x * kF * r2 / s)
+        return Pi_intra2(kF * x / s) * J1 * J2 * x / s**2
+    #print r1, r2
+    I, eps = integrate.quad(f, 0.0, np.Inf, limit=300)
+    
+    #
+    # Now add the missing contribution:
+    #   The integral for 1/(q^2 + 1) can be found as 
+    #   Int q dq /(q^2 + 1) J_0(qr)  ~ K_0(r)
+    #   With two Bessel functions, we can apply addition theorem, 
+    #   which gives K_0(|r - r'|), averaged over angles
+    #
+    def f2(theta):
+        r = math.sqrt(r1**2 + r2**2 + 2 * r1 * r2 * math.cos(theta))
+        return special.kn(0, kF * r)
+    I2, eps2 = integrate.quad(f2, 0.0, math.pi)
+    I2 *= 1.0 / math.pi * 2.0 / 3.0 
+    
+    #print "In F2: ", I, I2
+    return (I + I2) * kF**3 / (4.0 * math.pi**2)
+
+def F_intra(r1, r2, kF):
+    """
+        Integration routine --- for internal use in Q_intra
+        Here, we calculate intraband contribution in more direct
+        way. This routine is slow and less reliable than F_intra2
+        Provided mostly for testing F_intra2
+    """
+    s = max(kF * r1, kF * r2, 1.0)
+    def f(x):
+        J1 = special.jn(0, x * kF * r1 / s)
+        J2 = special.jn(0, x * kF * r2 / s)
+        return Pi_intra(kF * x / s) * J1 * J2 * x / s**2
+    I, eps = integrate.quad(f, 0.0, np.Inf, limit=10000)
+    I2 = 0.0
+    print "In F1:", I
+    return (I + I2) * kF**3 / (4.0 * math.pi**2)
+
+def testF(r1, r2, kF):
+    """
+        Test F_intra2 by comparing it to F_intra
+        The results should match to 1e-6 - 1e-8
+    """
+    F1 = F_intra(r1, r2, kF)
+    F2 = F_intra2(r1, r2, kF)
+    print "r1, 2 = ", r1, r2, "kF = ", kF
+    print "F1 = ", F1, "F2 = ", F2, "diff = ", F1 - F2, "rel: ", (F1 - F2)/(F1 + F2)*0.5
+    
+    
+def do_RPA_intra(r, kF):
+    """
+       Calculate the intraband kernel
+    """
+    Q1 = np.zeros((len(r), len(r)))
+    dr = np.zeros((len(r)))
+    dr[1:] = np.diff(r)
+    dr[0] = r[1] - r[0]
+    #if True: 
+    #   pylab.figure()
+    integrate_all = False
+    dr0 = 0.5
+    for i in range(len(r)):
+        print "Qintra:", i
+        for j in range(0, len(r) - 1):
+            #if i == j: continue
+           # if (j  == len(r) - 1):
+           #     r1 = r[-2]
+           #     r2 = r[-1]
+           # else:
+            r1 = r[j]
+            r2 = r[j + 1]
+            rc = (r1 + r2) / 2.0
+            dr = r2 - r1
+            #print r1, r2
+            
+            nj = int (dr / dr0) + 1
+            #print r[j], nj, dr, dr0, dr/dr0
+            Fk = np.zeros((nj,))
+            xk = np.linspace(r1, r2, nj + 2)[1:-1]
+            #Fk = np.zeros ((nj + 3,))
+            #xk = np.linspace(r1, r2, nj + 3)
+            for k in range(nj):
+                Fk[k] = F_intra2(r[i], xk[k], kF) * xk[k]  
+            dxk = dr / nj
+            
+            I1 = sum(Fk) * dxk
+            I2 = sum(Fk * (xk - rc)) * dxk
+            
+            Q1[i, j]     += 0.5 * I1 - I2 / dr
+            Q1[i, j + 1] += 0.5 * I1 + I2 / dr            
+            if False: #integrate_all:
+               def G1(x):
+                   #print "G1:", r[i], x, kF
+                   return F_intra2(r[i], x, kF) * x
+               def G2(x):
+                   #print "G2:", r[i], x, kF
+                   return F_intra2(r[i], x, kF) * x * (x - rc)
+               I1, eps1 = integrate.quad(G1, r1, r2)
+               I2, eps2 = integrate.quad(G2, r1, r2)
+               #if (j == len(r) - 1):
+               #    
+               #else:
+               Q1[i, j]     += 0.5 * I1 - I2 / dr
+               Q1[i, j + 1] += 0.5 * I1 + I2 / dr
+            #else: if False:
+            #   Fij = F_intra2(r[i], rc, kF)
+            #   Q1[i, j]     += 0.5 * Fij * r[j] * dr
+            #   Q1[i, j + 1] += 0.5 * Fij * r[j] * dr
+            #Fij = F_intra2(r[i], r[j], kF)
+            #Q1[i, j] = Fij * r[j] * dr[j]
+            #Q1[j, i] = Fij * r[i] * dr[i]
+        print "sum: ", sum(Q1[i, :]) * 4.0 * math.pi**2 # must be one
+        if False and i % 20 == 0:
+           import pylab
+           pylab.figure()
+           pylab.plot(r, Q1[i, :], label='r = %g' % r[i])
+           pylab.show()
+    return - Q1 * 2.0 * math.pi
+
+def RPA_intra(r, kF):
+    
+    integrate_all = False
+    """
+       Attempt to load the intraband kernel from file, 
+       calculate if unavailable
+    """
+    fname = "rpakernel-intra-kF=%g.dat.npz" % kF
+    try: 
+        data = np.load(fname)
+        print "Intraband kernel loaded from", fname
+        assert linalg.norm(r - data['r']) < 1e-6, "r grids are different"
+        return data['Q']
+    except:
+        import traceback
+        traceback.print_exc()
+        print "cannot load data from", fname, "; recalculating"
+        Q = do_RPA_intra(r, kF)
+        np.savez(fname, r=r, Q=Q, kF=kF)
+        return Q
+        
+        
+
+def F_inter(r1, r2, eps):
+    """
+       Calculate the interband kernel
+    """
+    def f(theta):
+        R = math.sqrt(eps**2 + r1*r1 + r2*r2 - 2 * r1 * r2 * math.cos(theta))
+        return (1.0 - 3.0*eps**2/R**2) / R**3
+    I, epsI = integrate.quad(f, 0.0, math.pi)
+    return I / 16.0  / math.pi
+
+def mk_inter_spline():
+    """
+       Construct spline interpolation of F_inter
+       as a function of r1/r2 (r1 < r2), 
+       and a function that reuses this interpolation
+    """
+    def F(x):
+        return F_inter(x, 1.0, 1e-3)
+    xvals = np.arange(0.0001, 1.0001, 0.001)
+    yvals = np.vectorize(F)(xvals)
+    spl = interpolate.splrep(xvals, yvals)
+    def Q_spline(r1, r2):
+        if (r1 <= r2):
+            x = r1 / r2
+            r = r2
+        else:
+           x = r2 / r1
+           r = r1
+        y = interpolate.splev(x, spl, der=0)
+        return y / r**3
+    return Q_spline
+    
+def do_RPA_inter(r):
+    """
+       Calculate the intraband kernel
+    """
+    integrate_all = False
+    Qs = mk_inter_spline()
+    N = len(r)
+    Q = np.zeros ((N, N))
+    for i in range (0, N):
+        print "Qinter: ", i
+        ri = r[i]
+        for j in range (0, i):
+            r1 = r[j]
+            r2 = r[j + 1]
+            dr = r2 - r1
+            rc = (r1 + r2)/2.0
+            def f1(rx):
+                return Qs(ri, rx)
+            def f2(rx):
+                return Qs(ri, rx) * (rx - rc)
+            if integrate_all or (abs(i - j) < 10) or (i < 20):
+                 I1, eps1 = integrate.quad(f1, r1, r2)
+                 I2, eps2 = integrate.quad(f2, r1, r2)
+            else:
+                 I1 = Qs(ri, rc) * dr
+                 qs1 = Qs(ri, r1)
+                 qs2 = Qs(ri, r2)
+                 I2 = (qs2 - qs1) * dr**2 / 12.0
+            Q[i, j]     += ( I1 / 2.0 - I2 / dr ) * r1 
+            Q[i, j + 1] += ( I1 / 2.0 + I2 / dr ) * r2
+        
+        for j in range (i + 1, N):
+            x1 = 1.0/r[j]
+            x2 = 1.0/r[j - 1]
+            drho = x2 - x1
+            r1 = r[j - 1]
+            r2 = r[j]
+            rhoc = (x1 + x2)/2.0
+            def f3(rhox):
+                return Qs(ri, 1.0/rhox) / rhox**3 
+            def f4(rhox):
+                return Qs(ri, 1.0/rhox) / rhox**3 * (rhox - rhoc)
+            if integrate_all or (abs(i - j) < 10) or (i < 20):
+                I3, eps3 = integrate.quad(f3, x1, x2)
+                I4, eps4 = integrate.quad(f4, x1, x2)
+            else:
+                I3  = Qs(ri, 1.0/rhoc) / rhoc**3 * drho
+                qs1 = Qs(ri, 1.0/x1) / x1**3
+                qs2 = Qs(ri, 1.0/x2) / x2**3
+                I4 = (qs2 - qs1)*drho**2/12.0
+            Q[i, j - 1] += ( I3 / 2.0 + I4 / drho) 
+            Q[i, j]     += ( I3 / 2.0 - I4 / drho) 
+    
+        def f5(rx):
+            return Qs(ri, rx)
+        def f6(rx):
+            return Qs(ri, rx) * rx
+    
+        r1 = r[0]
+        r2 = r[1]
+        dr = r2 - r1
+        I5, eps5 = integrate.quad(f5, 0.0, r[0])
+        I6, eps6 = integrate.quad(f6, 0.0, r[0])
+        Q[i, 0] += (  I5*r2/dr - I6/dr) * r1 
+        Q[i, 1] += ( -I5*r1/dr + I6/dr) * r2
+    
+        def f7(rhox):
+            return Qs(ri, 1.0/rhox) / rhox**3
+        def f8(rhox):
+            return Qs(ri, 1.0/rhox) / rhox**2
+    
+        x1 = 1.0/r[-1]
+        x2 = 1.0/r[-2]
+        I7, eps7 = integrate.quad(f7, 0.0, x1)
+        I8, eps8 = integrate.quad(f8, 0.0, x1)
+        drho = x2 - x1
+        Q[i, -1] += (   + I7*x2/drho - I8/drho)  
+        Q[i, -2] += (   - I7*x1/drho + I8/drho)  
+        
+        s = np.dot(Q[i, :], 1.0/r)
+        Q[i, i] -= r[i] * s
+        s = np.dot(Q[i, :], 1.0/r)
+        s1 = np.dot(Q[i, :], r/r)
+        print "s: ", s, s1
+    return Q
+
+def RPA_inter(r):
+    """
+       Attempt to load interband kernel from file, 
+       recalculate if the data is not available
+    """
+    fname = "rpakernel-inter.dat.npz"
+    try: 
+        data = np.load(fname)
+        print "Intraband kernel loaded from", fname
+        assert linalg.norm(r - data['r']) < 1e-6, "r grids are different"
+        return data['Q']
+    except:
+        import traceback
+        traceback.print_exc()
+        print "cannot load data from", fname, "; recalculating"
+        Q = do_RPA_inter(r)
+        np.savez(fname, r=r, Q=Q)
+        return Q
+
+if __name__ == '__main__':
+   testF(1.0, 2.0, 1.0)
+   testF(1.0, 10.0, 1.0)
+   testF(1.0, 1.1, 1.0)
+   
+   show_pi()
+   
+   rmin = 0.01
+   rmax = 30.0
+   N = 100
+   r = rmin * np.exp(math.log(rmax/rmin)/(N - 1.0) * np.arange(0, N, 1.0))
+   #r = np.arange(0.01, 10.0, 0.01)
+   kF = 1.0
+   Q_intra  = RPA_intra(r, kF)
+   Q_inter = RPA_inter(r)
+   r_0 = 1.0
+   U = 1.0 / (r**2 + r_0**2)**0.5
+   def Uq(q):
+       return 2.0 * math.pi / (q + 1e-8) * math.exp(-q*r_0)
+   
+   def rho_direct(x):
+       s = 1.0; #max(1.0, kF * x)
+       def f(q):
+           return Uq(q/s) * Pi_full(q/s, kF) * special.jn(0, q * x / s) * q / s**2
+       I1, eps1 = integrate.quad(f, 0, 2.0, limit=500)
+       I2, eps2 = integrate.quad(f, 2.0, 4.0, limit=500)
+       I3, eps3 = integrate.quad(f, 4.0, np.inf, limit=500)
+       I = I1 + I2 + I3
+       return - I / 2.0 / math.pi
+   
+   rho_RPA = np.dot(Q_inter, U)
+   rho_TH = -1.0 / 16.0 / (r**2 + r_0**2)**1.5
+   rho_intra = np.dot(Q_intra, U)
+   rho_th_intra = -U * abs(kF) / 2.0 / math.pi
+   rho_d = np.vectorize(rho_direct)(r)
+   import pylab
+   pylab.plot (r, rho_RPA, label='Numerics-kf=0')
+   pylab.plot (r, rho_RPA + rho_intra, label='Numerics-kf=%g' % kF)
+   pylab.plot (r, rho_TH,  label='Exact-kf=0')
+   pylab.plot(r, -U*abs(kF)/2.0/math.pi, label='TF kf=%g' % kF)
+   pylab.plot(r, rho_d, label='Direct integration')
+   pylab.legend()
+   pylab.figure()
+   pylab.loglog (r, np.abs(rho_RPA), label='Numerics')
+   pylab.loglog (r, np.abs(rho_RPA + rho_intra), label='Numerics-kf=%g' % kF)
+   pylab.loglog (r, np.abs(rho_TH),  label='Exact')
+   pylab.plot(r, abs(U)*abs(kF)/2.0/math.pi, label='TF kf=%g' % kF)
+   pylab.loglog(r, np.abs(rho_d), label='Direct integration')
+   pylab.legend()
+   pylab.show()
+   
