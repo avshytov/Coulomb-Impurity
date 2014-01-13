84dc9fd38928ddd067d4abac63d0cec8543afcf2
avshytov <avshytov@gmail.com>
2014-01-13 01:07:57 +0000

    Better parameter values in denssim.py

--- denssim.py
+++ denssim.py
@@ -9,18 +9,18 @@ import density
 import pylab as pl
 import coulomb
 
-N = 200
-Mmax = 11
-rmin = 0.02
-rmax = 40.0
+N = 500
+Mmax = 31
+rmin = 0.01
+rmax = 50.0
 
 r = util.make_lin_grid(rmin, rmax, N)
 Ef = 0.0
 B0 = 0.0
-Ecut = -1.5
+Ecut = -3.0
 Z = 0.5
 r_0 = 1.0
-tau_u_set = iteration.make_tau_set(3, 0.2, 0.001)
+tau_u_set = iteration.make_tau_set(3, 0.2, 0.1)
 print "taus: ", tau_u_set
 tau_rho_set = list(tau_u_set)
 Uext = Z / np.sqrt(r**2 + r_0**2)
@@ -29,7 +29,7 @@ rho_ext = -Z / 16.0 / np.sqrt(r**2 + r_0**2)**3
 graphene = density.GrapheneResponse (r, Ef)
 
 Nf = 4
-alpha = 1.0
+alpha = 0.9
 
 def rho_U(U, r):
     return graphene.rho_U(U) * Nf * alpha
