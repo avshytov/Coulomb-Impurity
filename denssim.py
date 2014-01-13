131c7a1a459c4da336e1f0e03300431c388424ae
avshytov <avshytov@gmail.com>
2014-01-13 11:24:43 +0000

    Changed the definition of the external potential: U = -Z*alpha/r

--- denssim.py
+++ denssim.py
@@ -18,18 +18,20 @@ r = util.make_lin_grid(rmin, rmax, N)
 Ef = -1e-4
 B0 = 0.0
 Ecut = -3.0
-Z = 0.5
+Z = 2.0
 r_0 = 1.0
+Nf = 4
+alpha = 0.9
+
+
 tau_u_set = iteration.make_tau_set(3, 0.2, 0.1)
 print "taus: ", tau_u_set
 tau_rho_set = list(tau_u_set)
-Uext = Z / np.sqrt(r**2 + r_0**2)
-rho_ext = -Z / 16.0 / np.sqrt(r**2 + r_0**2)**3  
+Uext = Z * alpha / np.sqrt(r**2 + r_0**2)
+rho_ext = -Z * alpha / 16.0 / np.sqrt(r**2 + r_0**2)**3  
 
 graphene = density.GrapheneResponse (r, Ef, Ecut=Ecut, Mmax=Mmax)
 
-Nf = 4
-alpha = 0.9
 
 def rho_U(U, r):
     return graphene.rho_U(U) * Nf * alpha
