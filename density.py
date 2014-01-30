Merge, fixed conflict, changed the sign of rho_ext in denssim.py

density.py
@@ -42,8 +42,8 @@ class RPA_tot:
        self.Q_inter = Kernel(rexp, Q_inter)
        Q_intra = np.zeros((len(r), len(r)))
        if (kF * r.max() > 0.01):
-           Q_intra = mkrpa2.RPA_intra(r, kF)
-       self.Q_intra = Kernel(r, Q_intra)
+           Q_intra = mkrpa2.RPA_intra(rexp, kF)
+       self.Q_intra = Kernel(rexp, Q_intra)
        
     def __call__ (self, r, U): 
         return self.Q_inter(r, U) + self.Q_intra(r, U)
@@ -54,9 +54,9 @@ class RPA_m:
        self.Qm_inter = Kernel(rexp, Qm_inter)
        Qm_intra = np.zeros((len(r), len(r)))
        if (kF * r.max() > 0.01):
-           Qm_intra = rpam.kernel_m_intra(r, mlist, kF) 
+           Qm_intra = rpam.kernel_m_intra(rexp, mlist, kF, '4') 
            #rpakernel.kernel_m_intra(r, mlist, kF)
-       self.Qm_intra = Kernel(r, Qm_intra)
+       self.Qm_intra = Kernel(rexp, Qm_intra)
     def __call__ (self, r, U): 
         return self.Qm_inter(r, U) + self.Qm_intra(r, U)
         
@@ -223,6 +223,30 @@ if __name__ == '__main__':
    Z = 0.02
    r_0 = 1.0
    U = Z / np.sqrt(r**2 + r_0**2)
+   #def Ur(rx):
+   #    if rx > r_0: 
+   #       return -Z/rx 
+   #    return -Z / r_0
+   #U = np.vectorize(Ur)(r)
+   def rho_b(rx):
+       #if rx > r_0: return 0.0; 
+       #return Z / math.pi / r_0**2
+       return r_0**2 / math.pi / (rx**2 + r_0**2)**2 * Z
+      
+   
+   rexp = util.make_exp_grid(0.001, 50.0, 1000)
+   Qcoul = Kernel(rexp, coulomb.kernel(rexp))
+   rho_bare = np.vectorize(rho_b)(r)
+   #U = Qcoul(r, rho_bare)
+   #U[0:3] = U[3]
+   import pylab as pl
+   pl.plot (r, U)
+   pl.plot (r, Z / np.sqrt(r**2 + r_0**2))
+   pl.figure()
+   pl.loglog(r, np.abs(U))
+   pl.loglog(r, Z / np.sqrt(r**2 + r_0**2))
+   pl.show ()
+   #rho_th = - math.pi / 8.0 * rho_bare
    rho_th = -Z * r_0 / 16.0 / np.sqrt(r**2 + r_0**2)**3
    
    rho_RPA = graphene.rho_RPA(U)
@@ -247,14 +271,16 @@ if __name__ == '__main__':
        Qths = []
        print 'Total Charge Calculation: rmin=', rmin, 'rmax=', rmax
        #for Z0 in [0.1]: 
-       for Z0 in [0.1, 0.2, 0.3, 0.4, 0.5]:
+       #for Z0 in [-0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5]:
+       for Z0 in np.linspace(-0.5, 0.5, 41):
        #for Z0 in [0.6, 0.8, 1.0, 1.2, 1.4]:
            print 'Calculating for Z0=',Z0 
            Ugrid.append(Z0)
            Qtheory = (( r_0 / np.sqrt(r_0**2+rmin**2))
                       - (r_0/ np.sqrt(r_0**2 + rmax**2)))
            Qtheory *= Z0 * np.pi / 8.0
-           Us = Z0 / np.sqrt(r**2 + r_0**2)
+           #Us = Z0 / np.sqrt(r**2 + r_0**2)
+           Us = Z0 / Z * U
            rhotot = graphene.rho_U(Us)
            
            rho_rpa = -Z0 / 16.0 * r_0 / np.sqrt(r**2 + r_0**2)**3
@@ -271,22 +297,30 @@ if __name__ == '__main__':
            Qrpa *= -2.0 * np.pi
            print "Z0 = ", Z0, "Qsim = ", Qsim, "Qrpa = ", Qrpa, "th: ", Qtheory
            import pylab as pl
-           pl.figure()
-           pl.title('Total density for Z0=%g'%Z0)
-           pl.plot(r,rhotot, label='total charge density')
-           pl.plot(r,rho_rpa, label='RPA charge density')
-           pl.plot(r, rho_rpa - rhotot, label='diff')
-           pl.legend()
-           pl.figure()
-           pl.title('Total density for Z0=%g'%Z0)
-           pl.plot(r,r*rhotot, label='r*total charge density')
-           pl.plot(r,r*rho_rpa, label='r*RPA charge density')
-           pl.plot(r, r*(rho_rpa - rhotot), label='r*diff')
-           pl.legend()
-           pl.show()
-           
+           if False:
+              pl.figure()
+              pl.title('Total density for Z0=%g'%Z0)
+              pl.plot(r,rhotot, label='total charge density')
+              pl.plot(r,rho_rpa, label='RPA charge density')
+              pl.plot(r, rho_rpa - rhotot, label='diff')
+              pl.legend()
+              pl.figure()
+              pl.title('Total density for Z0=%g'%Z0)
+              pl.plot(r,r*rhotot, label='r*total charge density')
+              pl.plot(r,r*rho_rpa, label='r*RPA charge density')
+              pl.plot(r, r*(rho_rpa - rhotot), label='r*diff')
+              pl.legend()
+              pl.show()
+           f = open('rho-Z=%g.dat' % (Z0), 'w')
+           for i in range(len(r)):
+               f.write('%g\t%g\n' % (r[i], rhotot[i]))
+           f.close()
            Qtots.append(Qsim)
            Qths.append(Qtheory)
+       f = open("Q-data.dat", 'w')
+       for i in range(len(Ugrid)):
+           f.write("%g\t%g\t%g\n" % (Ugrid[i], Qtots[i], Qths[i]))
+       f.close()
        Ugrid = np.array(Ugrid)
        Q_sim = np.array(Qtots)
        Q_linear = np.array(Qths)

denssim.py
@@ -24,11 +24,11 @@ Nf = 4
 alpha = 0.9
 
 
-tau_u_set = iteration.make_tau_set(3, 0.2, 0.1)
+tau_u_set = iteration.make_tau_set(3, 0.1, 0.05)
 print "taus: ", tau_u_set
 tau_rho_set = list(tau_u_set)
 Uext = -Z * alpha / np.sqrt(r**2 + r_0**2)
-rho_ext = -Z * alpha / 16.0 / np.sqrt(r**2 + r_0**2)**3  
+rho_ext = Z * alpha / 16.0 / np.sqrt(r**2 + r_0**2)**3  
 
 graphene = density.GrapheneResponse (r, Ef, Ecut=Ecut, Mmax=Mmax)
 

genk.py
@@ -11,7 +11,7 @@ import rpam
 rmin = 0.01
 rmax = 50.0
 N = 500
-Mmax = 21
+Mmax = 31
 mlist = np.array(range(0, Mmax))
 rexp = util.make_exp_grid(rmin, rmax, N)
 rlin = util.make_lin_grid(rmin, rmax, N)
@@ -25,8 +25,8 @@ Q1 = mkrpa2.RPA_inter(rexp)
 #for kF in [0.1, 0.2, 0.3, 0.4, 0.05, 0.15, 0.25, 0.35, 0.5]:
 #for kF in [3.0, 0.1, 0.2, 0.3]:
 #for kF in [1.0, 0.05, 0.15, 0.25, 0.35]:
-for kF in [3.0]:
+for kF in [1.0]:
     Q_tot = mkrpa2.RPA_intra(rlin, kF)
-    #Q_m   = rpakernel.kernel_m_intra(rlin, mlist, kF)
-    Q_m   = rpam.kernel_m_intra(rlin, mlist, kF)
+    #Q_m   = rpakernel.kernel_m_intra(rlin, mlist, kF, '4')
+    Q_m   = rpam.kernel_m_intra(rlin, mlist, kF, '4')
     

mkrpa2.py
@@ -221,6 +221,52 @@ def testF(r1, r2, kF):
     print "F1 = ", F1, "F2 = ", F2, "diff = ", F1 - F2, "rel: ", (F1 - F2)/(F1 + F2)*0.5
     print "G = ", G, "diff = ", G - F1, "rel:", (G - F1)/(G + F1) * 0.5
 
+def quick_quad(F, r1, r2, nj):
+    s0 = 0.0; 
+    s1 = 0.0; 
+    if nj <= 3:
+       xi = [-0.7745966692414834, 0.0,               0.7745966692414834]
+       w  = [0.5555555555555556, 0.8888888888888888, 0.5555555555555556]
+    elif nj == 4: 
+       xi = [0.8611363115940526, 0.3399810435848562, -0.3399810435848562, -0.8611363115940526]
+       w  = [0.347854845137, 0.652145154863, 0.652145154863, 0.347854845137]
+    elif nj <= 7: 
+       xi = [0.906179845939, 0.538469310106, 0.0,          -0.538469310106, -0.906179845939]   
+       w  = [0.236926885056, 0.478628670499, 0.56888888888, 0.478628670499, 0.236926885056]
+    elif nj <= 13:
+       xi = [0.978228658146, 0.887062599768, 0.730152005574,
+             0.519096129207, 0.26954315595, 0.0,-0.26954315595, 
+             -0.519096129207, -0.730152005574,  -0.887062599768, 
+             -0.978228658146]
+       w = [ 0.0556685671162, 0.125580369465, 0.186290210928,
+             0.23319376459, 0.26280454451, 0.272925086778, 0.26280454451,  
+             0.233193764592, 0.186290210928, 0.125580369465, 
+             0.055668567116       
+            ]
+    else:
+       xi = [ 0.98799251802,  0.937273392401, 0.84820658341, 0.72441773136,  
+              0.570972172609, 0.39415134707,  0.201194093997, 0.0, 
+              -0.201194093997, -0.394151347078, -0.570972172609, 
+              -0.72441773136, -0.84820658341,  -0.937273392401, 
+              -0.98799251802
+            ]
+       w  = [ 0.0307532419961,0.0703660474881,0.107159220467,
+              0.139570677926, 0.166269205817, 0.186161000016,
+              0.198431485327, 0.202578241926, 0.198431485327,
+              0.186161000016, 0.166269205817, 0.139570677926,
+              0.107159220467, 0.0703660474881, 0.0307532419961
+            ]
+    rc = (r1 + r2) / 2.0
+    dr = r2 - r1
+    for xi_i, w_i in zip(xi, w):
+        r = rc + xi_i * dr / 2.0; 
+        Fr = F(r)
+        s0 += Fr * w_i
+        s1 += Fr * w_i * (r - rc)
+    s0 *= dr / 2.0; 
+    s1 *= dr / 2.0; 
+    return s0, s1
+                                                             
 def do_RPA_intra(r, kF):
     """
        Calculate the intraband kernel
@@ -236,6 +282,8 @@ def do_RPA_intra(r, kF):
     Gfun = mk_intra_spline(kF, max(r) * 2.0)
     for i in range(len(r)):
         print "Qintra:", i
+        def Fi(rx):
+            return rx * Gfun(r[i], rx)
         for j in range(0, len(r) - 1):
             #if i == j: continue
            # if (j  == len(r) - 1):
@@ -249,19 +297,21 @@ def do_RPA_intra(r, kF):
             #print r1, r2
             
             nj = int (dr / dr0) + 1
-            #print r[j], nj, dr, dr0, dr/dr0
-            Fk = np.zeros((nj,))
-            xk = np.linspace(r1, r2, nj + 2)[1:-1]
-            #Fk = np.zeros ((nj + 3,))
-            #xk = np.linspace(r1, r2, nj + 3)
-            for k in range(nj):
-                Fk[k] = Gfun(r[i], xk[k]) * xk[k]
-                #Fk[k] = F_intra2(r[i], xk[k], kF) * xk[k]  
-            dxk = dr / nj
+            if False:
+               #print r[j], nj, dr, dr0, dr/dr0
+               Fk = np.zeros((nj,))
+               xk = np.linspace(r1, r2, nj + 2)[1:-1]
+               #Fk = np.zeros ((nj + 3,))
+               #xk = np.linspace(r1, r2, nj + 3)
+               for k in range(nj):
+                   Fk[k] = Gfun(r[i], xk[k]) * xk[k]
+                   #Fk[k] = F_intra2(r[i], xk[k], kF) * xk[k]  
+               dxk = dr / nj
             
-            I1 = sum(Fk) * dxk
-            I2 = sum(Fk * (xk - rc)) * dxk
-            
+               I1 = sum(Fk) * dxk
+               I2 = sum(Fk * (xk - rc)) * dxk
+            else:
+               I1, I2 = quick_quad(Fi, r1, r2, nj)   
             Q1[i, j]     += 0.5 * I1 - I2 / dr
             Q1[i, j + 1] += 0.5 * I1 + I2 / dr            
             if False: #integrate_all:
@@ -363,6 +413,9 @@ def do_RPA_inter(r):
     Qs = mk_inter_spline()
     N = len(r)
     Q = np.zeros ((N, N))
+    r_0 = 2.0; 
+    u0 = 1.0 / np.sqrt(r**2 + r_0**2); 
+    rho0 = -1.0 * r_0 / 16.0 / np.sqrt(r**2 + r_0**2)**3; 
     for i in range (0, N):
         print "Qinter: ", i
         ri = r[i]
@@ -376,7 +429,7 @@ def do_RPA_inter(r):
             dv = v2 - v1
             vc = (v1 + v2)/2.0
             def f1(v):
-                return Qs(ri, ri * v)
+                return Qs(ri, ri * v) 
             def f2(v):
                 return Qs(ri, ri * v) * (v - vc) 
             if integrate_all or (abs(i - j) < 10) or (i < 20):
@@ -384,8 +437,8 @@ def do_RPA_inter(r):
                  I2, eps2 = integrate.quad(f2, v1, v2)
             else:
                  I1  = Qs(ri, ri * vc) * dv
-                 qs1 = Qs(ri, ri * v1)
-                 qs2 = Qs(ri, ri * v2)
+                 qs1 = Qs(ri, ri * v1) 
+                 qs2 = Qs(ri, ri * v2) 
                  I2  = (qs2 - qs1) * dv**2 / 12.0
             #print "<", i, j, I1 * ri, I2/dv * ri
             Q[i, j]     += ( I1 / 2.0 - I2 / dv ) * ri**2 * v1 
@@ -418,7 +471,7 @@ def do_RPA_inter(r):
             Q[i, j]     += ( I3 / 2.0 - I4 / du) * ri**2# * r2
     
         def f5(rx):
-            return Qs(ri, rx)
+            return Qs(ri, rx) 
         def f6(rx):
             return Qs(ri, rx) * rx
     
@@ -435,7 +488,6 @@ def do_RPA_inter(r):
         #I6 = I6a + I6b
         I5, eps5 = integrate.quad(f5, 0.0, r[0])
         I6, eps6 = integrate.quad(f6, 0.0, r[0])
-        
         if False and i < 5: 
            import pylab as pl
            rxvals = np.linspace(0.0, r[0], 100)
@@ -477,6 +529,7 @@ def do_RPA_inter(r):
         s0 = np.dot(Q[i, :], 1.0/r)
         s2 = np.dot(Q[i, :], r/r)
         Q[i, i] -= r[i] * s0  
+        #Q[i, i] += rho0[i] / u0[i] - np.dot(Q[i, :], u0) / u0[i]; 
         s = np.dot(Q[i, :], 1.0/r)
         s1 = np.dot(Q[i, :], r/r)
         print "s: ", s, s1, s0, s2

rpam.py
@@ -1,21 +1,9 @@
-using_c_code = False
-
-if using_c_code: 
-   
-   import _rpam
-   def do_kernel_m_intra(r, mlist, kF):
-       return _rpam.kernel_m_intra(list(r), list(mlist), kF)
-   
-else:
-   import rpakernel
-   def do_kernel_m_intra(r, mlist, kF):
-       return rpakernel.do_kernel_m_intra(r, mlist, kF)
-    
+import _rpam
 import numpy as np
 from scipy import linalg
 
-def kernel_m_intra(r, mlist, kF):
-    fname =  "data/rpa-m3-Rmin=%g-Rmax=%g-N=%g-Mmax=%d-kF=%g.npz" % (r.min(), r.max(), len(r), mlist[-1], kF)
+def kernel_m_intra(r, mlist, kF, version='3'):
+    fname =  "data/rpa-m%s-Rmin=%g-Rmax=%g-N=%g-Mmax=%d-kF=%g.npz" % (version, r.min(), r.max(), len(r), mlist[-1], kF)
     try: 
         data = np.load(fname); 
         print "Loading RPA m-resolved intraband kernel from", fname

testrpam.py
@@ -2,17 +2,16 @@ import rpam
 import rpakernel
 
 import numpy as np
-#r = np.linspace(0.01, 50.0, 500)
-#m = np.arange(0.0, 21.0, 1.0)
-r = np.linspace(0.1, 10.0, 100)
-m = np.arange(0.0, 2.0, 1.0)
+r = np.linspace(0.01, 50.0, 500)
+m = np.arange(0.0, 31.0, 1.0)
 print r
 print m
-kF = 1.0
+kF = 3.0
 
-Q = rpam.kernel_m_intra(r, m, kF)
+Q = rpam.kernel_m_intra(r, m, kF, '3')
 
-Q1 = rpakernel.kernel_m_intra(r, m, kF)
+#Q1 = rpakernel.kernel_m_intra(r, m, kF)
+Q1 = rpam.kernel_m_intra(r, m, kF, '4')
 
 import pylab as pl
 