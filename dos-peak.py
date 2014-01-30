511bb7eb1f5676a220415f6c462f642af2d36869
scott-taylor-condmat <srt207@exeter.ac.uk>
2014-01-30 16:57:29 +0000

    Search for peak inside energy window. dos vs r plotted at Emax

--- dos-peak.py
+++ dos-peak.py
@@ -10,21 +10,38 @@ import odedirac
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    Emin = float(sys.argv[2])
-    Emax = float(sys.argv[3])
+    Emin0 = float(sys.argv[2])
+    Emax0 = float(sys.argv[3])
     data=np.load(filename)
+    N0 = 200
+    rtest = 1.0
+    threshold = 1e-7
     r = data['r']
     U = data['U']
-    
+    itest = (r-rtest).argmin()
     Mmax = 10
-    Espace = np.linspace(Emin,Emax,200)
     mlist = np.arange(0.0, Mmax+0.5, 1.0)
-    dos = odedirac.doscalc(Espace, r, U, mlist)
+
+    Emax = Emax0
+    Emin = Emin0
+
+    while (Emax - Emin) > threshold:
+        Espace = np.linspace(Emin,Emax,N0)
+        dos = odedirac.doscalc(Espace, r, U, mlist)
+        ipeak = dos[itest,:].argmax()
+        Emin = Espace[ipeak - 1]
+        Emax = Espace[ipeak + 1]
+
+    Epeak = Espace[ipeak]
+    print "Peak position =", Epeak
 
     figure()
-    for ri in [0.1, 1.0, 2.0, 3.0, 5.0, 10.0]:
-        i = np.abs(r - ri).argmin()
-        plot(Espace, dos[i, :], label='r = %g' % r[i])
-    legend()
-    
+    title('LDOS at Emax=%g' %Epeak)
+    plot(r,dos[:,ipeak])
     show()
+
+    if False:
+        figure()
+        plot(Espace, dos[itest, :], label='peak')
+        legend()
+        show()
